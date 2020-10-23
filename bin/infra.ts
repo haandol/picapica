8428924e60999fcd4b168377c1bf6b1e25cb30f5
Dongkyun Lee <dongkyl@amazon.com>
2020-10-23 22:29:17 +0900

    add stack props to config

--- bin/infra.ts
+++ bin/infra.ts
@@ -1,7 +1,8 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
 import * as cdk from '@aws-cdk/core';
+import { StackProps } from '../lib/interfaces/config';
 import { PicaPicaStack } from '../lib/picapica-stack';
 
 const app = new cdk.App();
-new PicaPicaStack(app, 'PicaPicaStack');
+new PicaPicaStack(app, 'PicaPicaStack', StackProps);
--- lib/interfaces/config.ts
+++ lib/interfaces/config.ts
@@ -1,13 +1,37 @@
 import { IConfig } from './interface';
 
-export const Config: IConfig = [
-  /* Add your own config
-  {
-    sourceName: 'test',
-    sourceRegion: 'us-west-2',
-    targetName: 'test',
-    targetRegion: 'ap-northeast-2',
-    branches: ['master'],
+export const Account = ''           // your account id
+export const Region = 'us-west-2';  // source region
+
+export const StackProps = {
+  env: {
+    account: Account,
+    region: Region,
   }
+};
+
+export const Config: IConfig = [
+  /*
+  {
+    sourceName: 'card-service',
+    sourceRegion: 'us-west-2',
+    targetName: 'card-service',
+    targetRegion: 'ap-northeast-2',
+    branches: ['mainline'],
+  },
+  {
+    sourceName: 'rule-service',
+    sourceRegion: 'us-west-2',
+    targetName: 'rule-service',
+    targetRegion: 'ap-northeast-2',
+    branches: ['mainline'],
+  },
+  {
+    sourceName: 'ecs-pipeline',
+    sourceRegion: 'us-west-2',
+    targetName: 'ecs-pipeline',
+    targetRegion: 'ap-northeast-2',
+    branches: ['mainline'],
+  },
   */
-]
+];
